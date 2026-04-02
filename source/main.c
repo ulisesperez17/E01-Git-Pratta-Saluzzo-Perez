05101d59a054e8a8154fdbf496be8a98c0ae9e1c
Ulises Pérez <ulisescayetanoperez@gmail.com>
2026-04-02 11:40:11 -0300

    titulo main

--- source/main.c
+++ source/main.c
@@ -1,2 +1,3 @@
-//codigo principal
+//La primera versión de la rama main debe contener el archivo main.c con los #include correspondientes, 
+//la función main y los prototipos de funciones primero(); segundo(); e iguales()
 
