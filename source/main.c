d1b22c58c0d0c9bef00d2645f7120a4f05d5f402
Ulises Pérez <ulisescayetanoperez@gmail.com>
2026-04-02 12:54:56 -0300

    Pedimos los números rama 1

--- source/main.c
+++ source/main.c
@@ -1,4 +1,26 @@
 //rama 1: debe solicitar al usuario que ingrese dos números y guardarlos en dos variables.
+#include <stdio.h>
+
+void primero();
+void segundo();
+void iguales();
+
+int main() {
+    int a, b;
+
+    return 0;
+
+printf("PROGRAMA QUE COMPARA DOS NÚMEROS.");
+    
+printf("Ingrese el primer numero: ");
+scanf("%d", &a);
+
+printf("Ingrese el segundo numero: ");
+scanf("%d", &b);
+
+}
+
+
 
 
 
