f15911151d8f7e95385e09c277262306075d2007
Ulises Pérez <ulisescayetanoperez@gmail.com>
2026-04-02 12:48:30 -0300

    primera versión rama main

--- source/main.c
+++ source/main.c
@@ -1,3 +1,14 @@
 //La primera versión de la rama main debe contener el archivo main.c con los #include correspondientes, 
 //la función main y los prototipos de funciones primero(); segundo(); e iguales()
 
+#include <stdio.h>
+
+void primero();
+void segundo();
+void iguales();
+
+int main() {
+    int a, b;
+
+    return 0;
+}
