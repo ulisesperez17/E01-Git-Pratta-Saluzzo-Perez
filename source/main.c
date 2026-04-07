191ee9b3a04fc1f66e24e8ea95779ca1b8009ffc
Martina Saluzzo <martinasaluzzo07@gmail.com>
2026-04-07 14:52:12 -0300

    comando comparacion

--- source/main.c
+++ source/main.c
@@ -1,2 +1,28 @@
 //rama 2 deberá comparar los números y llamar a la función correspondiente
+#include <stdio.h>
 
+void primero();
+void segundo();
+void iguales();
+
+int main() {
+    int a, b;
+
+    return 0;
+
+printf("PROGRAMA QUE COMPARA DOS NÚMEROS.");
+    
+printf("Ingrese el primer numero: ");
+scanf("%d", &a);
+
+printf("Ingrese el segundo numero: ");
+scanf("%d", &b);
+
+if (a > b)
+    primero();
+else if (b > a)
+    segundo();
+else
+    iguales();
+
+}
