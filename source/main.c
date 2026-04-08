97a53426c54affe546e29fc386ebb2310385aca0
Ulises Pérez <ulisescayetanoperez@gmail.com>
2026-04-08 09:38:30 -0300

    Modifico main

--- source/main.c
+++ source/main.c
@@ -4,7 +4,6 @@
 //rama 2 deberá comparar los números y llamar a la función correspondiente
 #include <stdio.h>
 //rama 3: deberá escribir cada función.
-#include <stdio.h>
 
 void primero();
 void segundo();
@@ -29,7 +28,6 @@ else
     iguales();
 
     return 0;
-}
 
 void primero(){
     printf ("El primer numero es mayor");
@@ -41,46 +39,7 @@ void iguales(){
     printf ("Ambos numeros son iguales");
 }
 
-
-
-
-void primero();
-void segundo();
-void iguales();
-
-int main() {
-    int a, b;
-
-    return 0;
-
-printf("PROGRAMA QUE COMPARA DOS NÚMEROS.");
-    
-printf("Ingrese el primer numero: ");
-scanf("%d", &a);
-
-printf("Ingrese el segundo numero: ");
-scanf("%d", &b);
-
 }
 
 
 
-
-
-#include <stdio.h>
-
-void primero();
-void segundo();
-void iguales();
-
-int main() {
-    int a, b;
-
-    return 0;
-if (a > b)
-    primero();
-else if (b > a)
-    segundo();
-else
-    iguales();
-}
